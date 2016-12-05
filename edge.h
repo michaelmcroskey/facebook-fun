076c1f52df02d54684c92b5e289fa3d537dbea27
Troy Prince <tprince@nd.edu>
2016-12-05 15:47:34 -0500

    edge

--- edge.h
+++ edge.h
@@ -9,12 +9,14 @@ struct edge {
     string target;
     bool type;
     int v;
-    //vector<>
-edge(string s, string t, bool ty) : source(s), target(t), type(ty), v(1) {} 
-
+    edge(string s, string t, bool ty) : source(s), target(t), type(ty), v(1) {} 
 
     //overloaded operator< for the priority queue to work
     //friend bool operator<(const edge left, const edge right) { return left.v > right.v; }
+};
 
-
+struct PEdgeCost {
+    bool operator()(const edge* lhs, const edge* rhs) const {
+	return lhs->v > rhs->v;
+    }
 };
