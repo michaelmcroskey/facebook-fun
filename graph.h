329d1c19d0cfcb74d12fb916e0efd7d56af08c70
Troy Prince <tprince@nd.edu>
2016-12-08 17:56:14 -0500

    updated graph.h

--- graph.h
+++ graph.h
@@ -13,25 +13,11 @@ struct Graph {
     map<string, vector<edge*> > adjacencyList;  
     priority_queue<edge*, vector<edge*>, PEdgeCost> F;
     map<string, string> marked;
-    /*Graph() {
-	string source;
-	string target;
-	while(cin >> source) {
-	    cin >> target;
-	    adjacencyList[source].push_back(new edge(source, target, false));
-	    adjacencyList[target].push_back(new edge(target, source, false));
-	}
-    }*/
+    
     void add(string source, string target, string type) {
-	//if(search_for_Node(source, target)) return;
-//	assert(search_for_Node(target, source));// && "Error because the path was not stored multi-directionally")
 	if(search_for_Node(source, target) != -1) return;
-	//assert(search_for_Node(target, source));// && "Error because the path was not stored multi-directionally"); 
-	//bool lineType;
-	//	if (type == "line") lineType = false;
-	//	else lineType = true;
-		adjacencyList[source].push_back(new edge(source, target, false));
-		adjacencyList[target].push_back(new edge(target, source, false));
+	adjacencyList[source].push_back(new edge(source, target, false));
+	adjacencyList[target].push_back(new edge(target, source, false));
     }
 
     bool search_for(string source) {
@@ -44,7 +30,7 @@ struct Graph {
     int search_for_Node(string source, string target) {
 	if(!search_for(source)) return -1;
 	else {
-	    for(size_t i=0; i< adjacencyList[source].size(); ++i) {
+	    for(size_t i=0; i < adjacencyList[source].size(); ++i) {
 		if (adjacencyList[source][i]->target == target) return i;
 	    }
 	}
@@ -52,63 +38,31 @@ struct Graph {
     }
 
     
-    
-    std::string write() {
-		string result;
-		string lineType;
-		result += "[";
-		bool first = true;
-		for (pair<string, vector<edge*> > edgeList : adjacencyList) {
-		    for (edge* E : edgeList.second) {
-				lineType = (E->type) ? "bold" : "line";
-				if (first){
-				    first = false;
-				} else {
-					result += ",";
-				}
-				result += "{\"source\":\"" + E->source;
-				result += "\",\"target\":\"" + E->target;
-				result += "\",\"type\":\"" + lineType + "\"}";
-		    }
+    string write() {
+	string result;
+	string lineType;
+	result += "[";
+	bool first = true;
+	for (pair<string, vector<edge*> > edgeList : adjacencyList) {
+	    for (edge* E : edgeList.second) {
+		lineType = (E->type) ? "bold" : "line";
+		if (first){
+		    first = false;
+		} else {
+		    result += ",";
 		}
-		result += "]";
-		return result;
+		result += "{\"source\":\"" + E->source;
+		result += "\",\"target\":\"" + E->target;
+		result += "\",\"type\":\"" + lineType + "\"}";
+	    }
+	}
+	result += "]";
+	return result;
     }
-
-    void prims() {
-	size_t N = adjacencyList.size();
-	//while(1) {
-	int dist=0;  //reset distance
-        //G = makeGraph(N);  //make graph from input
-	edge* E = adjacencyList.begin()->second[0]; //get starting node
-        addEdgesToF(E->target);//, F);  //begin with first in graph
-        marked[E->target] = E->source;
-        //loop until all nodes have been acounted for
-        int i = 0;
-        while(marked.size() < N) {
-            i++;
-	    //cout << i << endl;
-            if (i>1000) break;
-            //pop from frontier, pass over if its been recahed already
-            E = F.top();
-            F.pop();
-            if(marked.find(E->target) != marked.end()) continue;
-
-
-	    adjacencyList[E->source][search_for_Node(E->source, E->target)]->type = 1;
-            marked[E->target] = E->source;  //add to marked
-            addEdgesToF(E->target); //add this nodes edges to frontier
-            dist += E->v;  //increment distance
-        }
-
-	//clear marked and f
-        marked.clear();
-	F = priority_queue<edge*, vector<edge*>, PEdgeCost>();
-
-    }
+    
    
     void shortestPath(string start, string end) {
-      
+	bool connected = true;
         int dist=0;  //reset distance
         addEdgesToF(start); //begin start
 
@@ -117,10 +71,15 @@ struct Graph {
         while(1) {
             i++;
             ///cout << i << endl;
-            if (i>200) break;
-
+            if (i>10000) {
+		connected = false;
+		break;
+	    }
             //pop from frontier, pass over if its been recahed already
-	    if (F.empty()) break;
+	    if (F.empty()) {
+		connected = false;
+		break;
+	    }
             edge* E = F.top();
             F.pop();
             if(marked.find(E->target) != marked.end()) continue;
@@ -137,12 +96,17 @@ struct Graph {
 		//addEdgesToF(E->target); //add this nodes edges to frontier
             dist += E->v;  //increment distance
         }
-
-	while(start != end) {
-	    adjacencyList[end][search_for_Node(end, marked[end])]->type = 1;
-	    end = marked[end];
+	if(connected) {
+	    int j=0;
+	    while(start != end) {
+		//cout << end << ' ';
+		adjacencyList[end][search_for_Node(end, marked[end])]->type = 1;
+		end = marked[end];
+		//cout << end  << endl;
+		j++;
+		//if (j==100) break;
+	    }
 	}
-
         //clear marked and F
         marked.clear();
         F = priority_queue<edge*, vector<edge*>, PEdgeCost>();
@@ -152,7 +116,6 @@ struct Graph {
     void addEdgesToF(string source)  {
 	for(edge* E : adjacencyList[source]) {
 	    F.push(E);
-	    
 	}
     
     }
