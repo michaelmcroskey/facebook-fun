60a9eb848b8414b87388305a7232772616fa59ce
David <dmellitt@nd.edu>
2016-12-08 21:03:03 +0000

    Update dijkstras.cpp

--- dijkstras.cpp
+++ dijkstras.cpp
@@ -1,254 +1,101 @@
-<!DOCTYPE html>
-<html class="devise-layout-html" lang="en">
-<head prefix="og: http://ogp.me/ns#">
-<meta charset="utf-8">
-<meta content="IE=edge" http-equiv="X-UA-Compatible">
-<meta content="object" property="og:type">
-<meta content="GitLab" property="og:site_name">
-<meta content="Sign in" property="og:title">
-<meta content="GitLab.com" property="og:description">
-<meta content="https://gitlab.com/assets/gitlab_logo-7ae504fe4f68fdebb3c2034e36621930cd36ea87924c11ff65dbcb8ed50dca58.png" property="og:image">
-<meta content="https://gitlab.com/users/sign_in" property="og:url">
-<meta content="summary" property="twitter:card">
-<meta content="Sign in" property="twitter:title">
-<meta content="GitLab.com" property="twitter:description">
-<meta content="https://gitlab.com/assets/gitlab_logo-7ae504fe4f68fdebb3c2034e36621930cd36ea87924c11ff65dbcb8ed50dca58.png" property="twitter:image">
+// dijsktras.cpp
+// Michael McRoskey, David Mellitt, Troy Prince
 
-<title>Sign in · GitLab</title>
-<meta content="GitLab.com" name="description">
-<link rel="shortcut icon" type="image/x-icon" href="/assets/favicon-075eba76312e8421991a0c1f89a89ee81678bcde72319dd3e8047e2a47cd3a42.ico" />
-<link rel="stylesheet" media="all" href="/assets/application-14b7c53c199c382a20467d28b0f14bc785ae2c26c3b53b6585eddc0bace7776e.css" />
-<link rel="stylesheet" media="print" href="/assets/print-9c3a1eb4a2f45c9f3d7dd4de03f14c2e6b921e757168b595d7f161bbc320fc05.css" />
-<script src="/assets/application-061e3cee08b3422f46b7bb645fe4ca1af432b11620396206d42c81888c4611fd.js"></script>
-<meta name="csrf-param" content="authenticity_token" />
-<meta name="csrf-token" content="8kBggkRsdd4OGDa+BflpppXKdjW47QXUE9TGKFjEtGIi9i8t9jTU1zXr7hQGB2iVPxSHhfvFN7Fg6HjS2m3LEg==" />
-<meta content="origin-when-cross-origin" name="referrer">
-<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
-<meta content="#474D57" name="theme-color">
-<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-iphone-5a9cee0e8a51212e70b90c87c12f382c428870c0ff67d1eb034d884b78d2dae7.png" />
-<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-ipad-a6eec6aeb9da138e507593b464fdac213047e49d3093fc30e90d9a995df83ba3.png" sizes="76x76" />
-<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-iphone-retina-72e2aadf86513a56e050e7f0f2355deaa19cc17ed97bbe5147847f2748e5a3e3.png" sizes="120x120" />
-<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-ipad-retina-8ebe416f5313483d9c1bc772b5bbe03ecad52a54eba443e5215a22caed2a16a2.png" sizes="152x152" />
-<link color="rgb(226, 67, 41)" href="/assets/logo-d36b5212042cebc89b96df4bf6ac24e43db316143e89926c0db839ff694d2de4.svg" rel="mask-icon">
-<meta content="/assets/msapplication-tile-1196ec67452f618d39cdd85e2e3a542f76574c071051ae7effbfde01710eb17d.png" name="msapplication-TileImage">
-<meta content="#30353E" name="msapplication-TileColor">
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "graph.h"
+#include <memory>
+using namespace std;
 
+string exec(const char* cmd) {
+	char buffer[128];
+	std::string result = "";
+	std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+	if (!pipe) throw std::runtime_error("popen() failed!");
+	while (!feof(pipe.get())) {
+		if (fgets(buffer, 128, pipe.get()) != NULL)
+			result += buffer;
+	}
+	return result;
+}
 
-<!-- Piwik -->
-<script>
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//piwik.gitlab.com/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript><p><img src="//piwik.gitlab.com/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
+int main(int argc, char *argv[]) {
+	
+	// INPUT GRAPH FILE-----------------------------
+	
+	ifstream infile; 
+	infile.open("www/graph.txt");
+	string line;
+	Graph A;
+	
+	if (infile.is_open()){
+		while (getline(infile, line)){
+			string parsed = exec("./filter.sh");
+			parsed.erase(0,1);
+			parsed.pop_back();
+			
+			stringstream ss(parsed);
+			string dump, source, target, type;
+			while (ss >> dump >> source >> dump >> target >> dump >> type){
+				A.add(source, target, type);
+			}
+		}
+		infile.close();
+	}
+	
+	// INPUT FRIENDS TO SEARCH----------------------
+		
+	string friend_1, friend_2;
+	cin >> friend_1 >> friend_2;
+	
+	// Return error if incorrect input (doesn't exist or same person)
+	
+	string error = "SUCCESS";
+	
+	if (friend_1 == friend_2){
+		string same_person = "Error: " + friend_1 + " is the same person";
+		error = same_person;
+		cout << error;
+		return 0;
+	}
+		
+	if (A.search_for(friend_1)){
+		// Found friend_1
+		if (A.search_for(friend_2)){
+			// Found friend_2
+		} else {
+			string no_friend_2 = "Error: " + friend_2 + " does not exist in this network";
+			error = no_friend_2;
+			cout << error;
+			return 0;
+		}
+	} else if (A.search_for(friend_2)){
+		string no_friend_2 = "Error: " + friend_1 + " does not exist in this network";
+		error = no_friend_2;
+		cout << error;
+		return 0;
+	} else {
+		string no_friends = "Error: Neither " + friend_1 + " nor "+ friend_2 + " exists in this network";
+		error = no_friends;
+		cout << error;
+		return 0;
+	}
+		
+	// PERFORM DIJSKTRAS----------------------------
+	
+	A.shortestPath(friend_1, friend_2);
+	
+	cout << "SUCCESS";
+	
+	// RE-WRITE FILE WITH BOLD LINKS----------------
+	
+	ofstream rewrite;
+	rewrite.open("www/graph.txt", std::ofstream::trunc);
 
-
-</head>
-
-<body class="ui_charcoal login-page application navless" data-page="sessions:new">
-<div class="page-wrap">
-<script>
-//<![CDATA[
-window.gon={};gon.api_version="v3";gon.default_avatar_url="https:\/\/gitlab.com\/assets\/no_avatar-849f9c04a3a0d0cea2424ae97b27447dc64a7dbfae83c036c45b403392f0e8ba.png";gon.max_file_size=10;gon.relative_url_root="";gon.shortcuts_path="\/help\/shortcuts";gon.user_color_scheme="white";gon.award_menu_url="\/emojis";
-//]]>
-</script>
-<header class="navbar navbar-fixed-top navbar-empty">
-<div class="container">
-<div class="center-logo">
-<svg width="36" height="36" class="tanuki-logo">
-  <path class="tanuki-shape tanuki-left-ear" fill="#e24329" d="M2 14l9.38 9v-9l-4-12.28c-.205-.632-1.176-.632-1.38 0z"/>
-  <path class="tanuki-shape tanuki-right-ear" fill="#e24329" d="M34 14l-9.38 9v-9l4-12.28c.205-.632 1.176-.632 1.38 0z"/>
-  <path class="tanuki-shape tanuki-nose" fill="#e24329" d="M18,34.38 3,14 33,14 Z"/>
-  <path class="tanuki-shape tanuki-left-eye" fill="#fc6d26" d="M18,34.38 11.38,14 2,14 6,25Z"/>
-  <path class="tanuki-shape tanuki-right-eye" fill="#fc6d26" d="M18,34.38 24.62,14 34,14 30,25Z"/>
-  <path class="tanuki-shape tanuki-left-cheek" fill="#fca326" d="M2 14L.1 20.16c-.18.565 0 1.2.5 1.56l17.42 12.66z"/>
-  <path class="tanuki-shape tanuki-right-cheek" fill="#fca326" d="M34 14l1.9 6.16c.18.565 0 1.2-.5 1.56L18 34.38z"/>
-</svg>
-
-</div>
-</div>
-</header>
-
-
-
-<div class="container navless-container">
-<div class="content">
-<div class="flash-container flash-container-page">
-<div class="flash-alert">
-<div class="container-fluid container-limited">
-<span>You need to sign in or sign up before continuing.</span>
-</div>
-</div>
-</div>
-
-<div class="row">
-<div class="col-sm-5 pull-right new-session-forms-container">
-<div>
-<ul class="nav-links new-session-tabs nav-tabs" role="tablist">
-<li class="active" role="presentation">
-<a data-toggle="tab" href="#login-pane" role="tab">Sign in</a>
-</li>
-<li role="presentation">
-<a data-toggle="tab" href="#register-pane" role="tab">Register</a>
-</li>
-</ul>
-
-<div class="tab-content">
-<div class="login-box tab-pane active" id="login-pane" role="tabpanel">
-<div class="login-body">
-<form class="new_user gl-show-field-errors" aria-live="assertive" id="new_user" action="/users/sign_in" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="oOx0b/P7USzdhiC1PUq4sSpYDofbwR1iRsPXzWCvdJhwWjvAQaPwJeZ1+B8+tLmCgIb/N5jpLwc1/2k34gYL6A==" /><div class="form-group">
-<label for="login">Username or email</label>
-<input class="form-control top" autofocus="autofocus" autocapitalize="off" autocorrect="off" required="required" title="This field is required." type="text" name="user[login]" id="user_login" />
-</div>
-<div class="form-group">
-<label for="user_password">Password</label>
-<input class="form-control bottom" required="required" title="This field is required." type="password" name="user[password]" id="user_password" />
-</div>
-<div class="remember-me checkbox">
-<label for="user_remember_me">
-<input name="user[remember_me]" type="hidden" value="0" /><input type="checkbox" value="1" name="user[remember_me]" id="user_remember_me" />
-<span>Remember me</span>
-</label>
-<div class="pull-right forgot-password">
-<a href="/users/password/new">Forgot your password?</a>
-</div>
-</div>
-<div class="submit-container move-submit-down">
-<input type="submit" name="commit" value="Sign in" class="btn btn-save" />
-</div>
-</form>
-</div>
-</div>
-
-<div class="login-box tab-pane" id="register-pane" role="tabpanel">
-<div class="login-body">
-<form class="new_new_user gl-show-field-errors" aria-live="assertive" id="new_new_user" action="/users" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="9EAa2QVlfi9FpXFRYwBzw8qcKLLMp2foxhEWV+ti4t4k9lV2tz3fJn5Wqftg/nLwYELZAo+PVY21Laitacudrg==" /><div class="devise-errors">
-
-</div>
-<div class="form-group">
-<label for="new_user_name">Name</label>
-<input class="form-control top" required="required" title="This field is required." type="text" name="new_user[name]" id="new_user_name" />
-</div>
-<div class="username form-group">
-<label for="new_user_username">Username</label>
-<input class="form-control middle" pattern="[a-zA-Z0-9_\.][a-zA-Z0-9_\-\.]*[a-zA-Z0-9_\-]|[a-zA-Z0-9_]" required="required" title="Please create a username with only alphanumeric characters." type="text" name="new_user[username]" id="new_user_username" />
-<p class="validation-error hide">Username is already taken.</p>
-<p class="validation-success hide">Username is available.</p>
-<p class="validation-pending hide">Checking username availability...</p>
-</div>
-<div class="form-group">
-<label for="new_user_email">Email</label>
-<input class="form-control middle" required="required" title="Please provide a valid email address." type="email" value="" name="new_user[email]" id="new_user_email" />
-</div>
-<div class="form-group append-bottom-20" id="password-strength">
-<label for="new_user_password">Password</label>
-<input class="form-control bottom" required="required" pattern=".{8,}" title="Minimum length is 8 characters." type="password" name="new_user[password]" id="new_user_password" />
-<p class="gl-field-hint">Minimum length is 8 characters</p>
-</div>
-<div></div>
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-<div class="g-recaptcha" data-sitekey="6LfAERQTAAAAAL4GYSiAMGLbcLyUIBSfPrDNJgeC"></div>
-          <noscript>
-            <div style="width: 302px; height: 352px;">
-              <div style="width: 302px; height: 352px; position: relative;">
-                <div style="width: 302px; height: 352px; position: absolute;">
-                  <iframe
-                    src="https://www.google.com/recaptcha/api/fallback?k=6LfAERQTAAAAAL4GYSiAMGLbcLyUIBSfPrDNJgeC"
-                    frameborder="0" scrolling="no"
-                    style="width: 302px; height:352px; border-style: none;">
-                  </iframe>
-                </div>
-                <div style="width: 250px; height: 80px; position: absolute; border-style: none;
-                  bottom: 21px; left: 25px; margin: 0px; padding: 0px; right: 25px;">
-                  <textarea id="g-recaptcha-response" name="g-recaptcha-response"
-                    class="g-recaptcha-response"
-                    style="width: 250px; height: 80px; border: 1px solid #c1c1c1;
-                    margin: 0px; padding: 0px; resize: none;" value="">
-                  </textarea>
-                </div>
-              </div>
-            </div>
-          </noscript>
-
-<div>
-<input type="submit" name="commit" value="Register" class="btn-register btn" />
-</div>
-</form></div>
-</div>
-<div class="clearfix submit-container">
-<p>
-<span class="light">Didn't receive a confirmation email?</span>
-<a href="/users/confirmation/new">Request a new one</a>.
-</p>
-</div>
-
-</div>
-<div class="clearfix">
-<div class="omniauth-container">
-<p>
-<span class="light">
-Sign in with &nbsp;
-</span>
-<span class="light">
-<a class="oauth-image-link" data-no-turbolink="true" rel="nofollow" data-method="post" href="/users/auth/google_oauth2"><img alt="Google" title="Sign in with Google" src="/assets/auth_buttons/google_64-37d98b7033cc7059a484199ffdd766adf69babbc6e5fc64b43006ca866629f17.png" /></a>
-</span>
-<span class="light">
-<a class="oauth-image-link" data-no-turbolink="true" rel="nofollow" data-method="post" href="/users/auth/twitter"><img alt="Twitter" title="Sign in with Twitter" src="/assets/auth_buttons/twitter_64-86860edb139fb2f62fc25ef62a4213a5c8b20122fd8752ab0df09e740eb53deb.png" /></a>
-</span>
-<span class="light">
-<a class="oauth-image-link" data-no-turbolink="true" rel="nofollow" data-method="post" href="/users/auth/github"><img alt="GitHub" title="Sign in with GitHub" src="/assets/auth_buttons/github_64-84041cd0ea392220da96f0fb9b9473c08485c4924b98c776be1bd33b0daab8c0.png" /></a>
-</span>
-<span class="light">
-<a class="oauth-image-link" data-no-turbolink="true" rel="nofollow" data-method="post" href="/users/auth/bitbucket"><img alt="Bitbucket" title="Sign in with Bitbucket" src="/assets/auth_buttons/bitbucket_64-eceb0f98bfa1a0bdc088138f970e1bc209114a8934c1372b596e9d137e611c21.png" /></a>
-</span>
-</p>
-</div>
-
-</div>
-</div>
-
-</div>
-<div class="col-sm-7 brand-holder pull-left">
-<h1>
-GitLab.com
-</h1>
-
-<p dir="auto"><strong>GitLab.com offers free unlimited (private) repositories and unlimited collaborators, please sign up or in on the right.</strong></p>
-
-<ul dir="auto">
-<li>
-<a href="https://gitlab.com/explore/projects/trending">Explore projects on GitLab.com</a> (no login needed)</li>
-<li><a href="https://about.gitlab.com/gitlab-com/" rel="nofollow noreferrer" target="_blank">More information about GitLab.com</a></li>
-<li><a href="https://gitlab.com/gitlab-com/support-forum/issues">GitLab.com Support Forum</a></li>
-</ul>
-
-<p dir="auto">By signing up for and by signing in to this service you accept our:</p>
-
-<ul dir="auto">
-<li><a href="https://about.gitlab.com/privacy/" rel="nofollow noreferrer" target="_blank">Privacy policy</a></li>
-<li>
-<a href="https://about.gitlab.com/terms/#gitlab_com" rel="nofollow noreferrer" target="_blank">GitLab.com Terms</a>.</li>
-</ul>
-</div>
-</div>
-</div>
-</div>
-<hr class="footer-fixed">
-<div class="container footer-container">
-<div class="footer-links">
-<a href="/explore">Explore</a>
-<a href="/help">Help</a>
-<a href="https://about.gitlab.com/">About GitLab</a>
-</div>
-</div>
-</div>
-</body>
-</html>
+	if(rewrite.is_open()){
+		rewrite << A.write();
+	}
+	rewrite.close();
+}
